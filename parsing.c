4721bbe1fa86895e88bf501b27c6d4076381fc77
evans-manyala <evans.manyala@gmail.com>
2023-11-15 16:39:04 +0300

    Fixing errors

--- parsing.c
+++ parsing.c
@@ -4,126 +4,147 @@ struct func *parseline(char **ps, char *es);
 struct func *parseexec(char **ps, char *es);
 struct func *parseblock(char **ps, char *es);
 
+/**
+ * is_whitespace_or_symbol - Function checks for whitespace or symbols.
+ * @c: Character to be tested.
+ * Return: If whitespace or symbol.
+ *
+ * Description: Checks whether the given character (c)
+*/
 int is_whitespace_or_symbol(char c)
 {
-        static char whitespace[] = " \t\r\n\v";
-        static char symbols[] = "<|>&;()";
-        return strchr(whitespace, c) || strchr(symbols, c);
+	static const char whitespace[] = " \t\r\n\v";
+	static const char symbols[] = "<|>&;()";
+
+	return (strchr(whitespace, c) || strchr(symbols, c));
 }
+
+/**
+ * gettoken - Retrieves the next token from the input string.
+ * @ps: Pointer to the pointer input string.
+ * @es: Pointer to the End string.
+ * @q: Pointer to the beginning of the string.
+ * @eq: Pointer to the end of the string.
+ * Return: Retrieved token
+ *
+ * Description: Retrieves the next token from the input string.
+*/
 int gettoken(char **ps, char *es, char **q, char **eq)
 {
-        char *s, int ret;
+	char *s;
+	int ret;
 
-        s = *ps;
-        while (s < es && strchr(whitespace, *s))
-                s++;
-        if (q)
-                *q = s;
-        ret = *s;
-        switch (*s)
-        {
-        case 0:
-                break;
-        case '|':
-        case '(':
-        case ')':
-        case ';':
-        case '&':
-        case '<':
-                s++;
-                break;
-        case '>':
-                s++;
-                if (*s == '>')
-                {
-                        ret = '+';
-                        s++;
-                }
-                break;
-        default:
-                ret = 'a';
-                while (s < es && !is_whitespace_or_symbol(*s))
-                        s++;
-                break;
-        }
-        if (eq)
-                *eq = s;
+	s = *ps;
 
-        while (s < es && strchr(whitespace, *s))
-                s++;
-        *ps = s;
-        return ret;
+	while (s < es && strchr(whitespace, *s))
+		s++;
+		if (q)
+			*q = s;
+	ret = *s;
+	switch (*s)
+	{
+	case 0:
+		break;
+	case '|':
+	case '(':
+	case ')':
+	case ';':
+	case '&':
+	case '<':
+		s++;
+		break;
+	case '>':
+		s++;
+		if (*s == '>')
+		{
+			ret = '+';
+			s++;
+		}
+		break;
+	default:
+		ret = 'a';
+		while (s < es && !is_whitespace_or_symbol(*s))
+			s++;
+		break;
+	}
+	if (eq)
+		*eq = s;
+	while (s < es && strchr(whitespace, *s))
+		s++;
+	*ps = s;
+	return (ret);
 }
 int peek(char **ps, char *es, char *toks)
 {
-        char *s;
+	char *s;
 
-        s = *ps;
-        while (s < es && strchr(whitespace, *s))
-                s++;
-        *ps = s;
-        return *s && strchr(toks, *s);
+	s = *ps;
+	while (s < es && strchr(whitespace, *s))
+		s++;
+	*ps = s;
+	return (*s && strchr(toks, *s));
 }
 
 struct func *parseline(char **ps, char *es)
 {
-        struct func *func;
+	struct func *func;
 
-        func = parseexec(ps, es);
-        while (peek(ps, es, "&"))
-        {
-                gettoken(ps, es, 0, 0);
-                func = backfunc(func);
-        }
-        if (peek(ps, es, ";"))
-        {
-                gettoken(ps, es, 0, 0);
-                func = listfunc(func, parseline(ps, es));
-        }
-        return func;
+	func = parseexec(ps, es);
+	while (peek(ps, es, "&"))
+	{
+		gettoken(ps, es, 0, 0);
+		func = backfunc(func);
+	}
+	if (peek(ps, es, ";"))
+	{
+		gettoken(ps, es, 0, 0);
+		func = listfunc(func, parseline(ps, es));
+	}
+	return (func);
 }
 
 struct func *parseblock(char **ps, char *es)
 {
-        struct func *func;
+	struct func *func;
 
-        if (!peek(ps, es, "("))
-                panicerror("parseblock");
-        gettoken(ps, es, 0, 0);
-        func = parseline(ps, es);
-        if (!peek(ps, es, ")"))
-                panicerror("syntax - missing )");
-        gettoken(ps, es, 0, 0);
-        return func;
+	if (!peek(ps, es, "("))
+		panicerror("parseblock");
+	gettoken(ps, es, 0, 0);
+	func = parseline(ps, es);
+	if (!peek(ps, es, ")"))
+		panicerror("syntax - missing )");
+	gettoken(ps, es, 0, 0);
+	return (func);
 }
 
 struct func *parseexec(char **ps, char *es)
 {
-        char *q, *eq;
-        int tok, argc;
-        struct execfunc *func;
-        struct func *ret;
+	char *q, *eq;
+	int tok, argc;
 
-        if (peek(ps, es, "("))
-                return parseblock(ps, es);
+	struct execfunc *func;
+	struct func *ret;
 
-        ret = execfunc();
-        func = (struct execfunc *)ret;
+	if (peek(ps, es, "("))
+		return (parseblock(ps, es));
+	ret = execfunc();
+	func = (struct execfunc *)ret;
+	argc = 0;
 
-        argc = 0;
-        while (!peek(ps, es, "|)&;"))
-        {
-                if ((tok = gettoken(ps, es, &q, &eq)) == 0)
-                        break;
-                if (tok != 'a')
-                        panicerror("syntax");
-                func->argv[argc] = q;
-                func->eargv[argc] = eq;
-                argc++;
-                if (argc >= MAXARGS)
-                        panicerror("too many args");
-        }
-        func->argv[argc] = 0;
-        func->eargv[argc] = 0;
-        return ret;
+	while (!peek(ps, es, "|)&;"))
+	{
+		tok = gettoken(ps, es, &q, &eq);
+		if (tok == 0)
+			break;
+		if (tok != 'a')
+			panicerror("syntax");
+		func->argv[argc] = q;
+		func->eargv[argc] = eq;
+		argc++;
+		if (argc >= MAXARGS)
+			panicerror("too many args");
+	}
+	func->argv[argc] = 0;
+	func->eargv[argc] = 0;
+	return (ret);
 }
